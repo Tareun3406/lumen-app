851adf10c7c18342dfb64f11628bd7b6403e7c87
Tareun3406 <tareun3406@gmail.com>
2025-03-08 22:21:20 +0900

    chore: tamagui 적용

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
+import {TamaguiProvider} from "tamagui";
+import tamaguiConfig from "@/tamagui.config";
+import {DarkTheme, DefaultTheme, ThemeProvider} from "@react-navigation/native";
+import {useColorScheme} from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  const colorScheme = useColorScheme();
+
+  return (
+      <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+        <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
+          </Stack>
+        </ThemeProvider>
+      </TamaguiProvider>
+  );
 }
