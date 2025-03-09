50d831c7877efca0ce58dabb75aa71fcf49928dd
Tareun3406 <tareun3406@gmail.com>
2025-03-09 14:48:15 +0900

    chore: media query 설정

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,7 +1,12 @@
 import { defaultConfig } from '@tamagui/config/v4'
 import { createTamagui } from 'tamagui'
 
-export const tamaguiConfig = createTamagui(defaultConfig)
+export const tamaguiConfig = createTamagui({
+    ...defaultConfig,
+    media: {
+        md: {minWidth: 1024}
+    }
+})
 
 export default tamaguiConfig
 
