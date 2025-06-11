38508171f6549b7eccb1400d9810bb0263b014b9
박찬호 <ch.park@okestro.com>
2025-06-11 15:44:16 +0900

    feat: 비올라, 울프 토큰 최대치에서 클릭시 초기화 기능 추가

--- components/organisms/tokens/OneCounterToken.tsx
+++ components/organisms/tokens/OneCounterToken.tsx
@@ -6,11 +6,17 @@ import TokenToggleImg from "@/components/mole/TokenToggleImg";
 import styleSheet from "@/constants/styleSheet";
 import {useAppSelector} from "@/hooks/storeHooks";
 import {selectSettings} from "@/store/slices/settingsSlice";
+import {Pressable} from "react-native";
 
 export default function OneCounterToken(props: IActionProps) {
   const token = props.player.character.tokens[0];
-  const { setTokenToggle } = usePlayerAction(props);
+  const { setTokenToggle, setTokenCount } = usePlayerAction(props);
   const { flipPanel } = useAppSelector(selectSettings);
+
+  const pressToken = () => {
+    if (token.count < token.maxCount) return;
+    setTokenCount(0, 0);
+  }
 
   useEffect(() => {
     const payload = {
@@ -22,7 +28,9 @@ export default function OneCounterToken(props: IActionProps) {
 
   return (
     <XStack gap={"$1.5"} style={!props.player.isFirst && flipPanel?[styleSheet.flexReverse]:[]}>
-      <TokenToggleImg token={token} size={116} />
+      <Pressable onPress={pressToken}>
+        <TokenToggleImg token={token} size={116} />
+      </Pressable>
       <TokenCounterButton tokenIndex={0} player={props.player} />
     </XStack>
   )
