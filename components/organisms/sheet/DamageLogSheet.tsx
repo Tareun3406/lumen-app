61714c0a694c1c3b635b3dd04794baeca3112f98
Tareun3406 <tareun3406@gmail.com>
2025-06-11 23:40:51 +0900

    feat: 데미지 로그 가시성 개선

--- components/organisms/sheet/DamageLogSheet.tsx
+++ components/organisms/sheet/DamageLogSheet.tsx
@@ -1,7 +1,8 @@
-import {ScrollView, Sheet, SizableText, XStack, YStack} from "tamagui";
+import {isWeb, ScrollView, Sheet, SizableText, XStack, YStack} from "tamagui";
 import React, {useMemo} from "react";
 import {useAppSelector} from "@/hooks/storeHooks";
 import {selectDamageLogs} from "@/store/slices/boardSlice";
+import styleSheet from "@/constants/styleSheet";
 
 export interface DamageLogSheetProps{
   open: boolean,
@@ -32,15 +33,22 @@ export default function DamageLogSheet(props: DamageLogSheetProps) {
          padding="$4" alignItems="center" gap="$5" // justifyContent="center"
       >
        <ScrollView height={"100%"}>
-         <YStack style={{textAlign:"center"}}>
-           <SizableText>데미지 로그</SizableText>
+         <YStack style={[{textAlign:"center"}, styleSheet.centeredContainer]}>
+           <SizableText>데미지 기록</SizableText>
            {damageLogs && damageLogs.map((log, index) => {
              const sign = log.type === "DAMAGE" ? "-" : "+";
              return (
-               <XStack key={index}>
-                 <SizableText style={{textAlign:"right"}} width={"35vw"}>{log.isFirstPlayer && (sign + `${log.payload}(${log.result})`)}</SizableText>
-                 <SizableText style={{textAlign:"center"}} width={"30vw"}>{index}</SizableText>
-                 <SizableText style={{textAlign:"left"}} width={"35vw"}>{!log.isFirstPlayer && (sign + `${log.payload}(${log.result})`)}</SizableText>
+               <XStack key={index}  // @ts-ignore
+                       backgroundColor={index % 2 === 0 ? "$yellow4" : null}>
+                 <SizableText style={{textAlign:"right"}} width={isWeb ? "35vw" : "35%"} color={log.type === "DAMAGE" ? "red" : "green"}>
+                   {log.isFirstPlayer && (sign + `${log.payload}(${log.result})`)}
+                 </SizableText>
+                 <SizableText style={{textAlign:"center"}} width={isWeb ? "30vw" : "30%"} color={log.type === "DAMAGE" ? "red" : "green"}>
+                   {index}
+                 </SizableText>
+                 <SizableText style={{textAlign:"left"}} width={isWeb ? "35vw" : "35%"} color={log.type === "DAMAGE" ? "red" : "green"}>
+                   {!log.isFirstPlayer && (sign + `${log.payload}(${log.result})`)}
+                 </SizableText>
                </XStack>
              )
            }).reverse()}
